0b3610b2129bcbc16153e11f8a58aa81fa6df373
tuisemo <tuisemo@sina.cn>
2017-04-07 22:17:57 +0800

    add

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+require.config({
+	baseUrl:'js',
+    paths: {
+        "脚本jquery": './lib/jquery',
+        "脚本lodash": './lib/lodash',
+        "layer": './lib/layer',
+        "脚本MSG": './MSG',
+        "handlejs": './handlejs',
+        "jquery.fileupload": './jquery.fileupload',
+        "jquery.iframe-transport": './jquery.iframe-transport',
+        "jquery.ui.widget": './jquery.ui.widget',
+    }
+});
