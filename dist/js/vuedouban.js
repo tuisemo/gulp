1db2261cb8ab1a08cb7144b4347ad321e6c6bfed
tuisemo <tuisemo@sina.cn>
2017-04-26 00:39:10 +0800

    vueadd

--- dist/js/vuedouban.js
+++ dist/js/vuedouban.js
@@ -13,22 +13,24 @@ define(['脚本lazyload', 'vue'], function(lazyload, Vue) {
                 imgsrc: 'https://unsplash.it/1500/300/?random=' + Math.random(),
                 title: Math.random()
             }],
-            lists: [{
-                imgsrc: 'https://unsplash.it/150/150/?random=' + Math.random(),
-                title: Math.random()
-            }, {
-                imgsrc: 'https://unsplash.it/150/150/?random=' + Math.random(),
-                title: Math.random()
-            }, {
-                imgsrc: 'https://unsplash.it/150/150/?random=' + Math.random(),
-                title: Math.random()
-            }, {
-                imgsrc: 'https://unsplash.it/150/150/?random=' + Math.random(),
-                title: Math.random()
-            }, {
-                imgsrc: 'https://unsplash.it/150/150/?random=' + Math.random(),
-                title: Math.random()
-            }],
+            lists: [
+                /*{
+                                imgsrc: 'https://unsplash.it/150/150/?random=' + Math.random(),
+                                title: Math.random()
+                            }, {
+                                imgsrc: 'https://unsplash.it/150/150/?random=' + Math.random(),
+                                title: Math.random()
+                            }, {
+                                imgsrc: 'https://unsplash.it/150/150/?random=' + Math.random(),
+                                title: Math.random()
+                            }, {
+                                imgsrc: 'https://unsplash.it/150/150/?random=' + Math.random(),
+                                title: Math.random()
+                            }, {
+                                imgsrc: 'https://unsplash.it/150/150/?random=' + Math.random(),
+                                title: Math.random()
+                            }*/
+            ],
             inputfile: {
                 seen: false,
                 src: ""
@@ -40,8 +42,9 @@ define(['脚本lazyload', 'vue'], function(lazyload, Vue) {
         },
         methods: {
             moviesearch: function() {
-                var q=$('#search_key').val();
-                var tag=$('search_tag').val();
+                var that = this;
+                var q = $('#search_key').val();
+                var tag = $('search_tag').val();
                 $.ajax({
                     url: 'https://api.douban.com/v2/movie/search',
                     type: 'GET',
@@ -49,19 +52,26 @@ define(['脚本lazyload', 'vue'], function(lazyload, Vue) {
                     data: {
                         q: q,
                         tag: tag,
-                        start: start,
-                        count: count
+                        start: 0,
+                        count: 10
                     },
                     success: function(data) {
+                        for (var i = 0; i < data.subjects.length; i++) {
+                            imgsrc: data.subjects[i].casts[0].avatars.medium,
 
+                            that.lists.push({
+                                imgsrc: data.subjects[i].casts[0].avatars.medium,
+                                title: data.subjects[i].title
+                            })
+                        }
                     },
                     erroe: function() {}
-                })
+                });
 
             }
 
         }
     });
     window.app = app;
-    $('img.lazy').lazyload();
+    //$('img.lazy').lazyload();
 });
