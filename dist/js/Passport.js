1f3327a1e98a0e252852bbb6240c358563e05761
tuisemo <tuisemo@sina.cn>
2017-08-14 10:48:41 +0800

    cc

---
+++ dist/js/Passport.js
@@ -0,0 +1,433 @@
+define(['脚本tool', '脚本layer'], function() {
+    var Passport = function(ResultOpt) {
+        this.wait = 10;
+        this.timeBoo = true;
+        this.$userName = $("#userName");
+        this.$Tel = $("input[type='tel']");
+        this.$validateCode = $("#validateCode");
+        this.$sendmsgBtn = $("#sendmsg");
+        this.$msgCode = $("#msgCode");
+        this.$codeimg = $("#codeimg");
+        this.$reloadBtn = $("#reloadBtn");
+        this.$Password = $(".password");
+        this.$FPassword = $("#FPassword");
+        this.$CPassword = $("#CPassword");
+        this.$SubmitBtn = $("#submit");
+        this.$Check = $("#check");
+        this.$forgetInput = $("#forgetInput");
+        this.$ForgetBtn = $("#ForgetBtn");
+        /*=======企业注册dom======*/
+        this.$enterpriseName = $("#enterpriseName");
+        this.$licenseLocation = $("#licenseLocation");
+        this.$businessLicense = $("#businessLicense");
+        this.$organizationCode = $("#organizationCode");
+        this.$unifiedcreditCode = $("#unifiedcreditCode");
+        this.$certificateName = $("#certificateName");
+        this.$certificateNum = $("#certificateNum");
+        this.$EnBox1 = $("#EnBox1");
+        this.$EnBox2 = $("#EnBox2");
+        this.$idUpdate = $("input[name='idUpdate']");
+        this.init();
+    };
+    Passport.prototype = {
+        init: function() {
+            this.listen();
+        },
+        listen: function() {
+            var that = this;
+            that.$userName.on("blur", function() {
+                that.checkuserName();
+            }).on("focus", function() {
+                that.removeClass($(this));
+                $(this).parents('.form-group').find('.help-block').html(MSG["101"]);
+            });
+            that.$Tel.on("blur", function() {
+                that.checkTel();
+            }).on("focus", function() {
+                that.removeClass($(this));
+                $(this).parents('.form-group').find('.help-block').html(MSG["201"]);
+            });
+            that.$validateCode.on("focus", function() {
+                that.removeClass($(this));
+                $(this).parents('.input-group').next('.help-block').html(MSG["401"]);
+            });
+            that.$codeimg.on("click", function() {
+                that.reloadvalidate($(this));
+            });
+            //==========短信发送按钮监听============
+            that.$sendmsgBtn.on("click", function(event) {
+                that.sendMsgFor($(this).attr('data-type'), $(this).attr('data-domainname'));
+            });
+            that.$reloadBtn.on("click", function() {
+                that.reloadvalidate($(this));
+            });
+            that.$FPassword.on("blur", function() {
+                that.checkpassword();
+            });
+            that.$CPassword.on("blur", function() {
+                that.confirmpwd();
+            });
+            that.$Password.on("focus", function() {
+                that.removeClass($(this));
+                $(this).parents('.form-group').find('.help-block').html(MSG["501"]);
+            });
+            that.$SubmitBtn.on("click", function() {
+                that.SignUp();
+            });
+            that.$ForgetBtn.on("click", function() {
+                that.ForgetAccount();
+            });
+            /* =========企业注册=============*/
+            that.$idUpdate.on("click", function() {
+                that.Enterpriseinfo($(this).val());
+            });
+            that.$enterpriseName.on("focus", function() {
+                that.removeClass($(this));
+                $(this).parents('.form-group').find('.help-block').html(MSG["En100"]);
+            }).on("blur", function() {
+                that.REGEX(03, $(this));
+            });
+            that.$licenseLocation.on("focus", function() {
+                that.removeClass($(this));
+                $(this).parents('.form-group').find('.help-block').html(MSG["En200"]);
+            }).on("blur", function() {
+                that.REGEX(03, $(this));
+            });
+            that.$businessLicense.on("focus", function() {
+                that.removeClass($(this));
+                $(this).parents('.form-group').find('.help-block').html(MSG["En300"]);
+            }).on("blur", function() {
+                that.REGEX(02, $(this));
+            });
+            that.$organizationCode.on("focus", function() {
+                that.removeClass($(this));
+                $(this).parents('.form-group').find('.help-block').html(MSG["En400"]);
+            }).on("blur", function() {
+                that.REGEX(02, $(this));
+            });
+            that.$unifiedcreditCode.on("focus", function() {
+                that.removeClass($(this));
+                $(this).parents('.form-group').find('.help-block').html(MSG["En500"]);
+            }).on("blur", function() {
+                that.REGEX(02, $(this));
+            });
+            that.$certificateName.on("focus", function() {
+                that.removeClass($(this));
+                $(this).parents('.form-group').find('.help-block').html("");
+            }).on("blur", function() {
+                that.REGEX(03, $(this));
+            });
+            that.$certificateNum.on("focus", function() {
+                that.removeClass($(this));
+                $(this).parents('.form-group').find('.help-block').html("");
+            }).on("blur", function() {
+                that.checkIDnumber($(this).val());
+            });
+        },
+        //操作结果通用提示工具
+        optMsg: function(Obj, boolean, MSGnum) {
+            if (boolean === true) {
+                Obj.parents('.form-group').addClass('has-success')
+                    .find('.help-block').html(MSG["true"]);
+            } else {
+                Obj.parents('.form-group').addClass('has-error')
+                    .find('.help-block').html(MSG["false"] + MSG[MSGnum]);
+            }
+        },
+        //提示复位清除样式
+        removeClass: function(Obj) {
+            Obj.parents('.form-group').removeClass("has-success has-warring has-error");
+        },
+        //正则格式检验
+        REGEX: function(type, Obj) {
+            var that = this;
+            var value = $(Obj).val();
+            var regex1 = /^[0-9]+$/, //限制数字
+                regex2 = /^[0-9a-zA-Z._]*$/g, //限制字母+数字
+                regex3 = /[a-zA-Z|\u4e00-\u9fa5]/, //限制字母+中文
+                regex4 = /(^[a-zA-Z_\u4e00-\u9fa5]+[0-9]*)/; //限制字母+数字
+            switch (type) {
+                case 01:
+                    {
+                        if (!regex1.test(value)) {
+                            that.optMsg(Obj, false, 'REG');
+                        }
+                        break;
+                    }
+                case 02:
+                    {
+                        if (!regex2.test(value)) {
+                            that.optMsg(Obj, false, 'REG');
+                        }
+                        break;
+                    }
+                case 03:
+                    {
+                        if (!regex3.test(value)) {
+                            that.optMsg(Obj, false, 'REG');
+                        }
+                        break;
+                    }
+            }
+        },
+        Timesetter: function(o) {
+            var that = this;
+            this.timeBoo = false;
+            if (that.wait === 0) {
+                $("#msgtimer").html("发送校验码");
+                $("#sendmsg").show();
+                $("#msgtimer").hide();
+                that.wait = 10;
+                that.timeBoo = true;
+            } else {
+                if (that.wait == 10) {
+                    $("#sendmsg").hide();
+                    $("#msgtimer").show();
+                }
+                $("#msgtimer").html(that.wait + "秒后再重试");
+                that.wait--;
+                setTimeout(function() {
+                    that.Timesetter(o);
+                }, 1000);
+            }
+        },
+        //用户名唯一性校验
+        checkuserName: function() { //定义功能函数
+            var that = this;
+            var userNameVal = that.$userName.val();
+            var userNameRE = /^[a-zA-Z][a-zA-Z0-9_]{2,19}$/;
+            if (!userNameRE.test(userNameVal)) {
+                that.optMsg(that.$userName, false, "102");
+            }
+        },
+        //手机唯一性校验
+        checkTel: function() {
+            var that = this;
+            var TelVal = that.$Tel.val();
+            var telVal = /^((13[0-9])|(14[0-9])|(15[0-9])|(17[2-9])|(18[0-9]))\d{8}$/;
+            if (!telVal.test(TelVal) || TelVal.length != 11) {
+                that.optMsg(that.$Tel, false, 202);
+            }
+        },
+        //刷新图片验证码
+        reloadvalidate: function() {
+            var that = this;
+            that.$validateCode.val("");
+            that.$codeimg.attr('src', 'http://www.ixm.gov.cn/dis/passport/authCode/show?' + Math.random());
+            validateTrue = false;
+        },
+        //密码安全性校验
+        checkpassword: function() {
+            var that = this;
+            var pwdVal = that.$FPassword.val();
+            var pwdRE = /[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/;
+            if (!pwdRE.test(pwdVal)) {
+                that.optMsg(that.$FPassword, false, 502);
+            } else if (pwdVal.length < 8 || pwdVal.length > 30) {
+                that.optMsg(that.$FPassword, false, 502);
+            } else {
+                $.ajax({
+                    url: '/api/checkUserPwd',
+                    type: 'GET',
+                    dataType: 'json',
+                    cache: false,
+                    data: {
+                        'userName': that.$userName.val() || '',
+                        'password': that.$FPassword.val()
+                    },
+                    success: function(data) {
+                        if (data.code == 200) {
+                            that.optMsg(that.$FPassword, true);
+                        } else {
+                            ResultOpt.msg(data);
+                            that.optMsg(that.$FPassword, false, 503);
+
+                        }
+                    }
+                });
+            }
+        },
+        //再次确认密码
+        confirmpwd: function() {
+            var that = this;
+            if (that.$FPassword.val() != that.$CPassword.val()) {
+                that.optMsg(that.$FPassword, false, 505);
+                that.optMsg(that.$CPassword, false, 505);
+            }
+        },
+        /*==================================*/
+        sendMsgFor: function(operationType, domainName, sendType) {
+            var that = this;
+            if (!(that.timeBoo)) {
+                return false;
+            } else {
+                $("#sendmsg").attr('disabled');
+                $.ajax({
+                        url: "/api/sendMsg",
+                        dataType: "json",
+                        async: true,
+                        cache: false,
+                        data: {
+                            "operationType": operationType,
+                            "domainName": domainName,
+                            "sendType": sendType,
+                            "mobile": that.$Tel.val() || "",
+                            "code": that.$validateCode.val() || "",
+                        },
+                        type: "POST",
+                        success: function(data) {
+                            if (data.code == 200) {
+                                $("#msgtimer").hide();
+                                $("#sendmsg").show();
+                                $("#mobilemsg").addClass('text-success').html(MSG["true"] + data.result);
+                                that.Timesetter();
+                                return;
+                            }
+                            ResultOpt.msg(data);
+                        },
+                        error: function(data) {
+                            layer.msg('请求出错，请稍后重试');
+                            return;
+                        }
+                    })
+                    .done(function(data) {
+                        $("#sendmsg").removeAttr('disabled');
+                        console.log('111');
+                    });
+            }
+        },
+        /*==================================*/
+        SignUp: function() {
+            var that = this;
+            if (!that.$Check.is(':checked')) {
+                layer.msg('请勾选通行证协议');
+                return;
+            }
+            var attributeValue = that.$userName.val() + ";" + that.$Tel.val();
+            $.ajax({
+                url: "/api/checkUserAttribute",
+                dataType: "json",
+                async: true,
+                data: {
+                    "attributeValue": attributeValue,
+                    "attributeName": "userName;mobile",
+                    "domainName": "Citizen"
+                },
+                type: "POST"
+            }).done(function(data) {
+                if (data.code == 200) {
+                    $('form').submit();
+                } else {
+                    ResultOpt.msg(data);
+                }
+            }).fail(function(error) {
+                console.log(error);
+                layer.msg('请求出错，请稍后重试');
+                return;
+            });
+        },
+        /*==================================*/
+        EnterpriseSignUp: function() {
+            var that = this;
+            if (!that.$Check.is(':checked')) {
+                layer.msg('请勾选通行证协议');
+                return;
+            }
+            if ($("input[name='idUpdate']:checked").val() == "true") {
+                var attributeValue = that.$userName.val() + ";" + that.$Tel.val() + ";" +
+                    that.$enterpriseName.val() + ";" + that.$unifiedcreditCode.val();
+                var attributeName = "userName;mobile;enterpriseName;unifiedcreditCode";
+            } else {
+                var attributeValue = that.$userName.val() + ";" + that.$Tel.val() + ";" +
+                    that.$enterpriseName.val() + ";" + that.$businessLicense.val() + ";" + that.$organizationCode.val();
+                var attributeName = "userName;mobile;enterpriseName;businessLicense;organizationCode";
+            }
+            $.ajax({
+                url: "/api/checkUserAttribute",
+                dataType: "json",
+                async: true,
+                data: {
+                    "attributeValue": attributeValue,
+                    "attributeName": attributeName,
+                    "code": that.$validateCode.val(),
+                    "domainName": "Enterprise"
+                },
+                type: "POST"
+            }).done(function(data) {
+                if (data.code == 200) {
+                    layer.load(2, {
+                        shade: [0.1, '#333'] //0.1透明度的白色背景
+                    });
+                    $('form').submit();
+                } else {
+                    ResultOpt.msg(data);
+                }
+            }).fail(function(error) {
+                console.log(error);
+                layer.msg('请求出错，请稍后重试');
+                return;
+            });
+        },
+        ForgetAccount: function(domainName) {
+            var that = this;
+            $.ajax({
+                url: '/api/checkUser',
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    "userName": that.$forgetInput.val(),
+                    "code": that.$validateCode.val(),
+                    "domainName": domainName
+                },
+                success: function(data) {
+                    if (data.code === 200) {
+                        $('form').submit();
+                    } else {
+                        ResultOpt.msg(data);
+                    }
+                },
+                error: function(error) {
+                    layer.msg('请求出错，请稍后重试');
+                    return;
+                }
+            });
+        },
+        //身份证号格式检测
+        checkIDnumber: function(IDnum) {
+            var that = this;
+            if (IDnum.length != 18) {
+                that.optMsg(that.$certificateNum, false, 'IdReg');
+                return false;
+            }
+            if (IDnum[17] == 'x') {
+                IDnum = IDnum.replace('x', 'X');
+            }
+            //权重数组
+            var IDweight = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
+            //校验码数组
+            var IDcheckArray = ['1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'];
+            var IDsum = 0;
+            var IDmod;
+            for (var i = 0; i <= 16; i++) {
+                IDsum += IDnum[i] * IDweight[i];
+            }
+            IDmod = IDsum % 11;
+            if (IDnum[17] == IDcheckArray[IDmod]) {
+                return true;
+            } else {
+                that.optMsg(that.$certificateNum, false, 'IdReg');
+            }
+        },
+        Enterpriseinfo: function(idUpdate) {
+            var that = this;
+            if (idUpdate == "true") {
+                that.$EnBox1.show();
+                that.$EnBox2.hide();
+            } else {
+                that.$EnBox1.hide();
+                that.$EnBox2.show();
+            }
+        },
+    };
+    window.Passport = new Passport();
+});
---
+++ dist/jsp/signup.jsp
@@ -0,0 +1,123 @@
+<%@page import="com.sdp.util.RequestUtil"%>
+<%@ page language="java" import="java.util.*" pageEncoding="utf-8"%>
+<!-- include jstl标签库 -->
+<%@ include file="../include/include.jsp" %>
+<%
+    String fromCoAppName = RequestUtil.getParameterAndTrim(request, "fromCoAppName");
+    request.getSession().setAttribute("fromCoAppName", fromCoAppName);
+%>
+<%@page import="com.chinaums.xm.macUtil.DESPlus"%>
+<!DOCTYPE html>
+<html lang="zh-CN">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>通行证</title>
+    <!-- inject:base:css -->
+    <!-- endinject -->
+    <!--IEhack@include('./include/IEhack.html')-->
+</head>
+
+<body>
+    @@include('./include/header.html')
+    <div class="ixm-container wrap">
+        <div class="ixm-body">
+            <div class="col-sm-12 container-top">市民通行证注册</div>
+            <div class="col-sm-5 col-sm-offset-1 col-xs-12">
+                <p class="fr pad-r15"><a class="text-danger" href="/dis/passport/realNameRegUI">港澳侨台外籍注册点击此处</a></p>
+                <form class="form-horizontal col-sm-12 col-xs-12" action="/dis/passport/reg" method="post">
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label">用&nbsp;户&nbsp;名:</label>
+                        <div class="col-sm-9">
+                            <input id="userName" name="userName" type="text" class="form-control" placeholder="用户名">
+                            <span class="help-block">以英文字母开头，可包含3-20个字符（例：abc123）</span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label">手&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;机:</label>
+                        <div class="col-sm-9">
+                            <input name="mobile" type="tel" class="form-control" id="Tel" placeholder="建议使用省内手机">
+                            <span class="help-block">请输入省内有效手机号码 (不支持170/171/175号段)</span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label">图片验证:</label>
+                        <div class="col-sm-9">
+                            <div class="input-group">
+                                <input id="validateCode" name="validateCode" type="text" class="form-control" placeholder="图片验证码">
+                                <div class="input-group-btn">
+                                    <img id="codeimg" class="btn btn-default" src="http://www.ixm.gov.cn/dis/passport/authCode/show" style="padding:0;height:34px;">
+                                    <div id="reloadBtn" class="btn btn-default">刷新</div>
+                                </div>
+                            </div>
+                            <span class="help-block"></span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label">短信验证:</label>
+                        <div class="col-sm-9">
+                            <div class="input-group ">
+                                <input id="msgCode" name="msgCode" type="text" class="form-control" placeholder="请输入校验码">
+                                <span class="input-group-btn">
+                            <div id="sendmsg" class="btn btn-primary" data-type="01" data-domainname="Citizen" style="display:inline-block;">发送校验码</div>
+                            <div id="msgtimer" class="btn btn-default" style="display:none;" disabled="">发送校验码</div>
+                        </span>
+                            </div>
+                            <span class="help-block"></span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label">设置密码:</label>
+                        <div class="col-sm-9">
+                            <input name="password" type="password" class="form-control password" id="FPassword" placeholder="设置密码">
+                            <span class="help-block">8-30位字符包含数字和英文字符</span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label">确认密码:</label>
+                        <div class="col-sm-9">
+                            <input name="password" type="password" class="form-control password" id="CPassword" placeholder="确认密码">
+                            <!-- <span class="help-block">8-30位字符包含数字和英文字符</span> -->
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-3 col-sm-9">
+                            <div class="checkbox">
+                                <label>
+                                    <input id="check" type="checkbox" checked="checked"><small>我已阅读并接受 <a href="/" target="_blank">“市民通行证”服务协议</a></small>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-3 col-sm-9">
+                            <div id="submit" class="btn btn-primary col-sm-12">提交注册</div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-3 col-sm-9">
+                            <span class="fl"><a href="/forgetPwd.html" target="_blank"><small>找回密码</small>
+                            </a></span>
+                            <span class="fr"><a href="/forgetPwd.html" target="_blank"><small>去登录</small>
+                            </a></span>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-sm-4 col-sm-offset-2 hidden-xs">
+                <div class="warm-text">
+                    <h3>温馨提示：</h3>
+                    <p>市民通行证为您提供统一权威的实名电子身份，注册实名后不仅可以在“i厦门”惠民服务平台上享受在线办理积分入学、交通违法查询与缴交、生育险在线申领等来自多个部门的办事服务，还可以登录到其他如厦门交警网、人力资源和社会保障局网、厦门图书馆等打通了市民通行证登录的系统。随着越来越多的应用服务系统改造成市民通行证通行系统，未来您的市民通行证将在厦门“一个ID，一次登录，全市通行”。</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    @@include('./include/footer.html')
+    <!-- inject:none:js -->
+    <!-- endinject -->
+    @@include('./include/JSinclude.html')
+</body>
+
+</html>
